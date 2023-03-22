92ab708bbced4e01abb131e076f4e87b7e550069
jmarshall25 <jordanmarshall25@yahoo.com>
2023-03-22 23:37:52 +0000

    grocerylist

---
+++ Code-Class-08.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# Script: Ops 301 Class 08 Solution
+# Author: Jordan Marshall
+# Date of latest revision: 22Mar23
+# Purpose: Python Collections
+
+# Main
+
+# Assign to a variable a list of ten string elements
+grocerylist = ["eggs", "milk", "juice", "cereal", "bread", "chicken", "water", "napkins", "butter", "fish"]
+
+# Print the fourth element of the list
+print(grocerylist[4])
+
+# Print the sixth through tenth element of the list
+print(grocerylist[6:10])
+
+# Change the value of the seventh element to onion
+grocerylist[7] = 'onion'
+print(grocerylist)
+
+# End
