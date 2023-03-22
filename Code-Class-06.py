d4074e6415458c27b920ee90debab6807b78f965
Jordan Marshall <121899029+Jmarshall25@users.noreply.github.com>
2023-03-22 02:13:41 -0400

    Update and rename Code-Class-06.sh to Code-Class-06.py

---
+++ Code-Class-06.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+# Jordan Marshall
+# Code Challenge 06
+
+import os
+My_name = os.system("whoami")
+print(my_name)
+Address = os.system("ip a")
+print(Address)
+extract = os.system(lshw -short)
+print(extract
