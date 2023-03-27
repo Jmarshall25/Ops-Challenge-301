6071fa6fd53c2b7415e4f3bf6f9fdcddb5246bb9
Jordan Marshall <121899029+Jmarshall25@users.noreply.github.com>
2023-03-27 10:11:32 -0400

    Update Code-Class-10.py

--- Code-Class-10.py
+++ Code-Class-10.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Script: Ops 301 Class 08 Solution
+# Script: Ops 301 Class 10 Solution
 # Author: Jordan Marshall
 # Date of latest revision: 24Mar23
 # Purpose: Python File Handling 
